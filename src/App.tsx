f3d19daf15de406d4c49245f3ad5778c30bcab5d
krystiano13 <crystianotv@gmail.com>
2024-07-18 11:21:01 +0200

    create routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./views/Login";
+import { Tasks } from "./views/Tasks";
+
 function App() {
   return (
     <>
-      <button className="btn btn-secondary">Siema</button>
+      <div className="w-[100vw] h-[100vh]">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/tasks" element={<Tasks />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
