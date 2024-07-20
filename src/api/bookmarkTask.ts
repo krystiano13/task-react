2db4fbeff120f2798de3dee2221e3678165b07ce
krystiano13 <crystianotv@gmail.com>
2024-07-20 15:40:08 +0200

    add some additional cleanup

--- src/api/bookmarkTask.ts
+++ src/api/bookmarkTask.ts
@@ -4,7 +4,6 @@ import { createFetch } from "./helpers/createFetch";
 export async function bookmarkTask(info: { id: number; bookmark: boolean }) {
   if (!Cookies.get("user")) return;
   const user = JSON.parse(Cookies.get("user") as string);
-
   const url = `https://timtest.timenotes.io/api/v1/tasks/${info.id}/${
     info.bookmark ? "" : "un"
   }bookmark`;
